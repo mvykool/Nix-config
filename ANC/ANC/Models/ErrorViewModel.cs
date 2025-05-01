ab27e9d0524b500ab5e74e8b641198239a928ef2
Maicol Hernandez <maykolandres4@gmail.com>
2025-05-01 12:32:06 -0400

    init, and reset

--- ANC/ANC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ANC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
