3ddfe5bd8951a61012296bed1a38a96d55b0cdb2
Maicol Hernandez <maykolandres4@gmail.com>
2025-05-02 05:00:00 -0400

    added interactivity

--- main/src/components/topbar/TopbarNotification.tsx
+++ main/src/components/topbar/TopbarNotification.tsx
@@ -1,7 +1,7 @@
 export const TopbarNotification = () => {
   return (
     <>
-      <i className="bx bx-bell text-2xl relative">
+      <i className="bx bx-bell text-2xl relative hover:cursor-pointer">
         {" "}
         <div className="h-2 w-2 bg-red-500 rounded-full right-0 absolute top-0"></div>
       </i>
--- main/src/routes/_root.tsx
+++ main/src/routes/_root.tsx
@@ -1,7 +1,8 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { SidebarContainer } from "../components/sidebar/SidebarContainer";
-import { TopbarContainer } from "../components/topbar/TopBarContainer";
+import { TopbarContainer } from "../components/topbar/TopbarContainer";
+import { WalletContainer } from "../components/wallet/WalletContainer";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -13,7 +14,11 @@ function RootComponent() {
       <SidebarContainer />
       <div className="flex flex-col w-full">
         <TopbarContainer />
-        <Outlet />
+        <div className="flex w-full justify-between p-5">
+          {" "}
+          <Outlet />
+          <WalletContainer />
+        </div>
       </div>
       <TanStackRouterDevtools position="bottom-right" />
     </div>
