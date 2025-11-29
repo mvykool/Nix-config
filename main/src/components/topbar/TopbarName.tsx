9127a64b5672f01b5faf917430a6c963d74e80c6
Maicol Hernandez <maykolandres4@gmail.com>
2025-11-29 14:30:00 -0500

    delete old project

--- main/src/components/topbar/TopbarName.tsx
+++
@@ -1,7 +0,0 @@
-export const TopbarName = () => {
-  return (
-    <div>
-      <p className="text-xl font-semibold">Hello, User</p>
-    </div>
-  );
-};
--- main/src/types/windows.d.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { ExternalProvider } from "ethers";
-
-interface Window {
-  ethereum?: ExternalProvider | any;
-}
